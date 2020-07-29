9ecd592a0dd64b827a3d2cae70a8e688597c58e6
dignifiedquire <me@dignifiedquire.com>
2020-07-29 14:15:42 +0200

    feat(cli): improve help

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,28 @@ use notifiers::server;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
+    /// If set, this will use the sandbox servers, instead of the production ones.
     #[structopt(short, long)]
     sandbox: bool,
+    /// The message for the notification to be sent.
     #[structopt(long)]
     message: String,
+    /// Path to the certificate file.
     #[structopt(long, parse(from_os_str))]
     certificate_file: PathBuf,
+    /// Password for the certificate file.
     #[structopt(long)]
     password: String,
+    /// The topic for the notification.
     #[structopt(long)]
     topic: Option<String>,
+    /// The host on which to start the server.
     #[structopt(long, default_value = "127.0.0.1")]
-    server: String,
+    host: String,
+    /// The port on which to start the server.
     #[structopt(long, default_value = "9000")]
     port: u16,
+    /// The path to the database file.
     #[structopt(long, default_value = "notifiers.db", parse(from_os_str))]
     db: PathBuf,
 }
@@ -38,7 +46,7 @@ async fn main() -> Result<()> {
     let state2 = state.clone();
     let server =
         async_std::task::spawn(
-            async move { server::start(state2, opt.server.clone(), opt.port).await },
+            async move { server::start(state2, opt.host.clone(), opt.port).await },
         );
 
     async_std::task::spawn(async move {
